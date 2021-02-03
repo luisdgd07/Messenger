fa179909f858f450bba737c88fc8f99093dbad70
Luis David Gonzalez Duerto <luisdgd07@gmail.com>
2021-02-03 20:27:57 -0300

    Creación de proyecto

---
+++ src/components/SideBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="drawer" dark app mini-variant-width="100" class="white">
+            <v-list>
+                <v-list-item class="mb-16">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-dove</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-list>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-file-alt</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-calendar-day</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-comment-alt</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-badge color="blue" dot overlap>
+                        <v-list-item-content>
+                            <v-icon class="mb-2" large color="yellow">fa fa-bell</v-icon>
+                        </v-list-item-content>
+                    </v-badge>
+                </v-list-item>
+            </v-list>
+            <v-list style="position: absolute;bottom:0" class="ml-5" flat>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-users</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon class="mb-2" large color="blue">fa fa-cog</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-avatar class="mb-5">
+                        <v-badge  dot bottom color="green" offset-x="10" offset-y="10">
+                            <v-avatar size=40>
+                                <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+                            </v-avatar>
+                        </v-badge>
+                    </v-list-item-avatar>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+
+</template>
+<script>
+export default {
+    data:()=>({
+        drawer:true,
+    })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      selected:[2],
+        items:[ {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+          title: 'Ali Connors',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          subtitle: `Wish I could come, but I'm out of town this weekend.`,
+          title: 'me, Scrott, Jennifer',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          subtitle: 'Do you have Paris recommendations? Have you ever been?',
+          title: 'Sandra Adams',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          subtitle: 'Have any ideas about what we should get Heidi for her birthday?',
+          title: 'Trevor Hansen',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+          title: 'Britta Holt',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+          title: 'Connors',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          subtitle: `Wish I could come, but I'm out of town this weekend.`,
+          title: ' Jennifer',
+        },
+        {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          subtitle: 'Do you have Paris recommendations? Have you ever been?',
+          title: 'Sandra ',
+        },],
+        files:[
+          {text:'Landing page.zip',icon:'mdi-cloud-upload'},
+          {text:'requirements.pdf',icon:'mdi-cloud-upload'},
+          {text:'Pages.doc',icon:'mdi-cloud-upload'},
+        ],
+        panel:[2],
+        show:false,
+        message:'Escribe un mensaje',
+        marker:true,
+        iconIndex:0,
+
+    },
+    computed: {
+      
+    },
+  },
 }
 </script>
